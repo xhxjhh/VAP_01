722b48368eefe0c61f2fc7de531856a7653bc980
Jan Havlicek <jan.havlicek@student.sps-cl.cz>
2023-10-03 10:37:13 +0200

    Změna velkosti textu

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 alert("ahoj!!!")
+
+function fibCislo(n) {
+    if (n < 2) return 1
+        return fibCislo(n - 1) + fibCislo(n - 2)
+
+}
+
+function vypocitej() {
+    alert(fibCislo(Number(document.getElementById("vstup").value)))
+}
